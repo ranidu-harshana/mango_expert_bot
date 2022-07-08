83771d8952af91f5c456c0e5d7def09065f850d8
Ranidu Harshana <68012743+ranidu-harshana@users.noreply.github.com>
2022-07-08 14:47:10 +0530

    pdf

--- resources/views/user/planting-details-pdf.blade.php
+++ resources/views/user/planting-details-pdf.blade.php
@@ -5,8 +5,273 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+        u {
+            text-decoration: none;
+            border-bottom: 3px solid black;
+        }
+
+        p {
+            font-size: 22px;
+        }
+
+        .section {
+            margin: 6% 6% 0 6%
+        }
+        .container {
+            width: 0 auto;
+            text-align: center;
+        }
+
+        
+    </style>
 </head>
 <body>
-    <p>Hello</p>
+    <div class="container">
+        
+        <h1><u>Automated Fruit Plantation <br>Instruction Web Application</u></h1>
+        <br><br><br><br><br>
+        <h1><u>Mango planting manual</u></h1><br><br>
+        <img src="{{ public_path('assets/pdf_img/Picture1.png') }}" alt="">
+    </div>
+
+    <div class="section">
+        <p>Step 1: Assemble all required materials</p><br>
+        <p>Assemble all the necessary materials and equipment needed; grafted 
+            mango seedling(s) manure and or fertilizer, watering can and water, 
+            spade and a hoe.</p><br>
+
+
+        <p>Step 2: Selection of seedlings for planting</p><br>
+        <p>It is important to identify and select seedlings that are healthy and big 
+            enough for planting as they have a higher chance of survival. Quality 
+            planting material (grafted) is important for good production/ high yields. 
+            Selected seedlings should not show signs of pests and diseases.</p><br>
+
+        <p>Step 3: Transportation of seedlings for planting</p><br>
+        <p>Different means of transporting seedlings are used, depending on the 
+            distance from the nursery to the identified planting site. During
+            transportation of seedlings, take caution not to pile them up on each as 
+            this can cause damage to the young trees. It is recommended that the 
+            seedlings are transported upright in boxes, plastic crates or bags as this 
+            will reduce the chance of damaging them.</p><br>
+
+        <p>Step 4: Land preparation and planting</p>
+            <br>
+        <div style="width: 100%; margin-top: 60px">
+            <div style="width: 50%; float: left">
+                <p style="font-size: 14px">FOC@KDU</p>
+            </div>
+
+            <div style="width: 50%; float: right">
+                <p style="font-size: 14px; text-align: right">Chamath Pivithuru</p>
+            </div>
+        </div>
+            <br><br><br>
+        <div>
+            <p>An ideal climate for mango trees ranges from the humid tropical to the 
+                semi-arid subtropical, wherever a dry period exists of at least 3 to 4 
+                months and that have sufficient light to induce flowering.</p>
+            <p>Spacing between mango trees varies from variety to variety and growing 
+                environment (dry and wet zone). In dry zone the spacing varies from 10 
+                m x 10 m, because the vegetative growth is limited, while in wet and rich 
+                soils the preferred spacing is12 m x 12 m, because of abundant vegetative 
+                growth.</p>
+            <p>Planting holes should be dug before the onset of a rainy season where 
+                possible for water to collect in it to enhance the survival of the seedling 
+                planted. The holes should be dug to a depth of 1 meter, width of 1 meter 
+                and length of 1 meter (1m x 1m x 1m).
+                </p>
+            <p>The spacing of these holes should consider the mango tree canopy as well 
+                as the soil fertility of the area.
+                </p><br>
+            <div class="container">
+                <img src="assets/pdf_img/Picture2.png" alt="" width="50%">
+            </div>
+            
+            <p>Step 5: Planting the seedling</p>
+            <p>If ready to plant, re-fill the planting holes with a ¼ of the top soil from 
+                around the hole or with the soils removed when preparing the hole(s).
+                </p>
+
+            <div style="width: 100%">
+                <div style="width: 50%; float: left">
+                    <p style="font-size: 14px">FOC@KDU</p>
+                </div>
+    
+                <div style="width: 50%; float: right">
+                    <p style="font-size: 14px; text-align: right">Chamath Pivithuru</p>
+                </div>
+            </div>
+            <p>Where necessary, mix this soil with recommended amounts of farm yard 
+                manure or tree fertilizers thoroughly at the ratio of 3:1 respectively.</p>
+
+            <div style="width: 100%">
+                <div style="width: 50%; float: left">
+                    <img src="assets/pdf_img/Picture3.png" alt="" width="90%">
+                </div>
+    
+                <div style="width: 50%; float: right">
+                    <img src="assets/pdf_img/Picture4.png" alt="" width="90%">
+                </div>
+            </div>
+                <br>
+            <p>Remove the polythene bag or polythene tube by tearing on the side or 
+                below while holding the seedling up-right.</p>
+            <p>
+                However, if the seedling was raised in a tin or any other container, bang 
+                it from the top slightly.
+            </p>
+            <p>Then remove the seedling from the tube/ container with its soils by 
+                holding it at the base of the stem. Ensure there is no disturbance of root 
+                systems while doing this.</p>
+            <div style="width: 100%">
+                <div style="width: 50%; float: left">
+                    <img src="assets/pdf_img/Picture5.png" alt="" width="90%" height="200px">
+                </div>
+    
+                <div style="width: 50%; float: right">
+                    <img src="assets/pdf_img/Picture6.png" alt="" width="90%" height="200px">
+                </div>
+            </div>
+           <br>
+            <p>Place the seedling in the hole. Half fill the hole with top soil and press it 
+                gently towards the root. Fill the hole with water and allow it to drain 
+                before completely filling the hole with soils. Ensure this is done without
+                removing the soils around the roots and or bending the roots.
+                </p>
+
+            <div style="width: 100%; margin-top: 80px">
+                <div style="width: 50%; float: left">
+                    <p style="font-size: 14px">FOC@KDU</p>
+                </div>
+    
+                <div style="width: 50%; float: right">
+                    <p style="font-size: 14px; text-align: right">Chamath Pivithuru</p>
+                </div>
+            </div>
+
+        </div>
+
+        
+    </div>
+
+    <div style="margin: 0 6% 0 6%">
+        <p>Make a basin around the base of tree by gently pressing down the soils 
+        around the seedling. The basin will help hold the water after watering.</p>
+
+        <div style="width: 100%">
+            <div style="width: 50%; float: left">
+                <img src="assets/pdf_img/Picture7.png" alt="" width="90%" height="200px">
+            </div>
+
+            <div style="width: 50%; float: right">
+                <img src="assets/pdf_img/Picture8.png" alt="" width="90%" height="200px">
+            </div>
+        </div>
+        <br>
+        <p>Ensure that the seedling sits upright just as it was on the polythene tube 
+            in the nursery.</p>
+
+        <p>Apply a mulch layer under young trees. Mulch provides organic matter 
+            (a valuable source of tree nutrients and food for beneficial soil microorganisms), reduces moisture loss and competition from weeds</p>
+            <br>
+        <div style="width: 100%">
+            <div style="width: 50%; float: left">
+                <img src="assets/pdf_img/Picture9.png" alt="" width="90%" height="200px">
+            </div>
+
+            <div style="width: 50%; float: right">
+                <img src="assets/pdf_img/Picture10.png" alt="" width="90%" height="200px">
+            </div>
+        </div>
+        <p>.</p>
+        <div style="width: 100%; margin-top: 190px">
+            <div style="width: 50%; float: left">
+                <p style="font-size: 14px">FOC@KDU</p>
+            </div>
+
+            <div style="width: 50%; float: right">
+                <p style="font-size: 14px; text-align: right">Chamath Pivithuru</p>
+            </div>
+        </div>
+    </div>
+    
+    <div style="margin: 0 6% 0 6%">
+        <h2 style="margin-top: 10%"><u>Management of fruit tree after planting</u></h2><br>
+        <h3><u>Irrigation</u></h3>
+        <p>Watering should be done immediately after planting in the field to foster
+            proper establishment.</p>
+        <p>If possible; you can water your mango tree at interval of 8 to 12 days 
+            from when the mango tree start to produce flowers to maturity so as to 
+            have higher yields.</p>
+        <p>Note; watering is not recommended 2 to 3 months before flowering as it 
+            will encourage increased branching and leaves development.
+            </p>
+        <br>
+        <h3><u>Protection</u></h3>
+        <p>Where the fruit tree is planted on an open field particularly where 
+            animals are also grazed, it is advisable to protect the fruit tree against 
+            destruction by livestock, especially goats, by building a fence of sticks/ 
+            net around the planted fruit tree.</p>
+        <br>
+        <h3><u>Manure and Fertilizer application</u></h3>
+        <p>After about 3-6 months, start application of nitrogenous fertilizers (in 
+            case of fruit trees e.g. mango, pawpaw, oranges). Apply nitrogenous 
+            fertilizers in varied intervals to avoid leaching out (downward movement 
+            of useful mineral or nutrients in water). Alternatively, properly prepared 
+            farm yard manure or compost, kitchen ash (left from cooking) and 
+            mulching can be used in place of fertilizers.
+            </p>
+        <div style="width: 100%; margin-top: 80px">
+            <div style="width: 50%; float: left">
+                <p style="font-size: 14px">FOC@KDU</p>
+            </div>
+
+            <div style="width: 50%; float: right">
+                <p style="font-size: 14px; text-align: right">Chamath Pivithuru</p>
+            </div>
+        </div>
+    </div>
+
+    <div style="margin: 0 6% 0 6%">
+        <h3 style="margin-top: 10%"><u>Pruning and training</u></h3>
+        <p>The height and form of the fruit tree needs to be controlled. This is meant 
+            to guide the tree and facilitate its harvesting at its later stages. It is
+            advisable that you carry out pruning in the first year to guide tree into 
+            desired shape, and when trees are about 1 m from the ground, cap seedling 
+            to encourage side branches. Carry out pruning later for proper tree
+            maintenance and should be carried out after fruit harvest. Control height 
+            to about 3.5 m and all branches at knee level (about 0.5m) be pruned. 
+            Remove all dead branches to allow sunlight through canopy to the ground 
+            under the tree.
+            </p>
+            <br>
+        <h3><u>Flowering and fruit formation</u></h3>
+        <p>Grafted fruit trees usually start to flower within two years from planting. 
+            However, fruit formation should be discouraged at this stage as it can 
+            affect growth of the fruit tree. For grafted mangoes, it is advisable that 
+            fruit formation is allowed from the fourth year onwards.</p>
+            <br>
+        <h3><u>Pests and diseases</u></h3>
+        <p>Fruit fly is the most common pests for mangoes and other fruits usually 
+            causing a lot of damage to and high losses of these fruits. The use of fruit 
+            fly traps is popular method of controlling fruit fly among other ways. 
+            Other pests include gall midges and scales insects. Powdery mildew and 
+            anthracnose are the most common mango diseases mainly affecting the 
+            flowers, tender leaves and leading to development of black lesions on 
+            mango fruits. Powdery mildew can be controlled by spraying using 
+            Sulphur or Bayleton, while anthracnose can be controlled using copper
+            fungicides.</p>
+            <div style="width: 100%; margin-top: 50px">
+                <div style="width: 50%; float: left">
+                    <p style="font-size: 14px">FOC@KDU</p>
+                </div>
+    
+                <div style="width: 50%; float: right">
+                    <p style="font-size: 14px; text-align: right">Chamath Pivithuru</p>
+                </div>
+            </div>
+    </div>
 </body>
 </html>
